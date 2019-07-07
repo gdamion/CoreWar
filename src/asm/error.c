a4c5a508427f1e332465366e71074653af03bb27
George Damion lannister <gdamion-@at-r8.21-school.ru>
2019-07-07 13:22:20 +0300

    upd

--- src/asm/error.c
+++ src/asm/error.c
@@ -6,26 +6,27 @@
 /*   By: gdamion- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/06 11:57:52 by gdamion-          #+#    #+#             */
-/*   Updated: 2019/07/07 10:49:57 by gdamion-         ###   ########.fr       */
+/*   Updated: 2019/07/07 13:19:28 by gdamion-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include "com.h"
+#include "com.h"
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
 
 //here we clean all lists and allocated info
-void	free_info()
+void	free_info(t_data *data)
 {
 
 }
 
-void	error(char *err_place, int str_no, int col_no)
+void	error(char *err_place, int str_no, int col_no, t_data *data)
 {
-	ft_printf("Error: %s, string %d col %d\n", err_place, str_no, col_no);
-	fclose(); //close file
-	free_info();
+	//ft_printf("Error: %s, string %d col %d\n", err_place, str_no, col_no);
+	print_error(err_place);
+	fclose(data->fd); //close file
+	free_info(data);
 	exit(1);
 }
