7c365fecb1cc932d8c5b42b53eb603aa66fc0403
George Damion lannister <gdamion-@at-r8.21-school.ru>
2019-07-07 13:28:11 +0300

    upd

---
+++ src/asm/lexical.c
@@ -0,0 +1,84 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lexical.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gdamion- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/07 13:25:26 by gdamion-          #+#    #+#             */
+/*   Updated: 2019/07/07 13:27:18 by gdamion-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "com.h"
+
+void		skip_whitespaces(t_data *data, char *line)
+{
+	while(SP(line[data->cursor]))
+		data->cursor++;
+}
+
+void		skip_comment(t_data *data, char *line)
+{
+	if (line[data->cursor] == COMMENT_CHAR)
+		while(line[data->cursor] && line[data->cursor] != "\n")
+			data->cursor++;
+}
+
+void		get_str(t_data *data, char *line)
+{
+	int		i;
+	char	*temp;
+
+	i = 0;
+	temp = 0;
+	while (line[data->cursor] != '\"')
+		data->cursor++;
+	while (!ft_findchar(line + data->cursor, '\"')
+			&& get_next_line(data->fd, &temp))
+		ft_catpro(line, temp);
+	if ((i = ft_findchar(line + data->cursor, '\"')) > PROG_NAME_LENGTH)
+		error(ERR_CHNAME_LEN, {str}, {col}); //add str and col
+	token_add(data, STRING);
+	data->token->content = ft_strsub(line, data->cursor, i);
+}
+
+void		get_cmd(t_data *data, char *line)
+{
+	token_add(data, COMMAND);
+	if (!ft_strcmp(NAME_CMD_STRING, line))
+		data->token->content = NAME_CMD_STRING;
+}
+
+/*
+	while (ft_findchar(LABEL_CHARS, *(line + data->cursor)))
+		data->cursor++;
+*/
+
+void		tokenizing(t_data *data, char *line)
+{
+	if (line[data->cursor] == '.')
+		get_cmd(data, line);
+	else if (line[data->cursor] == '\"' && data->cursor++)
+		get_str(data, line);
+}
+
+void		lexical_analyzer(t_data *data)
+{
+	int		size;
+	char	*line;
+
+	while ((size = get_next_line(data->fd, &line))
+							&& !(data->cursor = 0))
+	{
+		if (size == -1)
+			error(ERR_READING);
+		skip_whitespaces(data, line);
+		skip_comment(data, line);
+		if (line[data->cursor])
+			tokenizing(data, line);
+		ft_strdel(&line);
+	}
+	if (size == -1)
+		error(ERR_READING);
+}
--- src/asm/syntaxical.c
+++ src/asm/syntaxical.c
@@ -1,28 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   error.c                                            :+:      :+:    :+:   */
+/*   syntaxical.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gdamion- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/07/06 11:57:52 by gdamion-          #+#    #+#             */
-/*   Updated: 2019/07/07 13:25:55 by gdamion-         ###   ########.fr       */
+/*   Created: 2019/07/07 13:25:02 by gdamion-          #+#    #+#             */
+/*   Updated: 2019/07/07 13:25:42 by gdamion-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "com.h"
-
-//here we clean all lists and allocated info
-void	free_info(t_data *data)
-{
-
-}
-
-void	error(char *err_place, int str_no, int col_no, t_data *data)
-{
-	//ft_printf("Error: %s, string %d col %d\n", err_place, str_no, col_no);
-	print_error(err_place);
-	fclose(data->fd); //close file
-	free_info(data);
-	exit(1);
-}
